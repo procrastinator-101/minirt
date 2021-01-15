bd4bfd0d76fbafd1ce09d316021182c15b495407
Younes Arroubi <yarroubi@e1r2p2.1337.ma>
2021-01-15 12:55:35 +0100

    scenes remodeling

--- configuration_reader/get_texture.c
+++ configuration_reader/get_texture.c
@@ -6,7 +6,7 @@
 /*   By: yarroubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 17:26:15 by yarroubi          #+#    #+#             */
-/*   Updated: 2020/12/29 11:10:58 by yarroubi         ###   ########.fr       */
+/*   Updated: 2021/01/15 11:03:12 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ int	get_texture(char *line, t_texture *texture, t_display *display, int type)
 	if (texture->type[0] == UV_MAP)
 	{
 		start = get_texture_map(line, &(texture->uv_map), display, start);
-		if (start == -1)
-			return (-type);
+		if (start < 0)
+			return (start);
 	}
 	else if (texture->type[0] == RGB || texture->type[0] == CHECKERBOARD)
 	{
 		start = get_texture_color(line, texture, start);
-		if (start == -1)
+		if (start < 0)
 			return (-type);
 	}
 	if (texture->type[1] == BUMP_MAP)
@@ -35,8 +35,8 @@ int	get_texture(char *line, t_texture *texture, t_display *display, int type)
 		if ((start = update_start(line, start)) == -1)
 			return (-type);
 		start = get_texture_map(line, &(texture->bump_map), display, start);
-		if (start == -1)
-			return (-type);
+		if (start < 0)
+			return (start);
 	}
 	return (check_end_entity_info(line, start, type));
 }
