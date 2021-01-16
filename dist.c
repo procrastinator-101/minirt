9882313c39d3ed1751405c2617382dc7b3501738
Younes Arroubi <yarroubi@e1r6p2.1337.ma>
2021-01-16 16:56:57 +0100

    solar system done

--- dist.c
+++ dist.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+	double x;
+	double y;
 	printf("enter x : ");
-	double x = scanf("%lf", &x);
+	scanf("%lf", &x);
 	printf("enter y : ");
-	double y = scanf("\n%lf", &y);
+	scanf("\n%lf", &y);
 	printf("dist = %lf\n", sqrt(x * x + y * y) * 2);
 }
