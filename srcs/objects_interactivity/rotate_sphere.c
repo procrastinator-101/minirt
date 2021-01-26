f9014b4d4d3b9b75766b521abe594fc6392eb845
Younes Arroubi <yarroubi@e1r12p12.1337.ma>
2021-01-26 12:46:28 +0100

    before rgb

--- srcs/objects_interactivity/rotate_sphere.c
+++ srcs/objects_interactivity/rotate_sphere.c
@@ -6,7 +6,7 @@
 /*   By: yarroubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 09:57:39 by yarroubi          #+#    #+#             */
-/*   Updated: 2020/12/27 12:00:42 by yarroubi         ###   ########.fr       */
+/*   Updated: 2021/01/26 12:46:13 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	rotate_sphere(t_sphere *sphere, t_smat_3d m)
 {
 	if (sphere->mode == STATIC_MODE)
 		return ;
-	if (sphere->texture.type[0] == INTERACTIVE_MODE && !sphere->texture.type[1])
+	if (sphere->texture.type[0] == RGB && !sphere->texture.type[1])
 		return ;
 	sphere->basis.u = smat_vec_3d_product(m, sphere->basis.u);
 	sphere->basis.v = smat_vec_3d_product(m, sphere->basis.v);
