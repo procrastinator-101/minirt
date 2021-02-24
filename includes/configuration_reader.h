3d3eb1311c85298933bcf309b7c3727caa3f62e1
Younes Arroubi <yarroubi@e1r5p2.1337.ma>
2021-02-24 17:00:00 +0100

    disk and rectangle in process 2

--- includes/configuration_reader.h
+++ includes/configuration_reader.h
@@ -6,7 +6,7 @@
 /*   By: yarroubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/23 21:27:58 by yarroubi          #+#    #+#             */
-/*   Updated: 2021/02/24 16:31:15 by yarroubi         ###   ########.fr       */
+/*   Updated: 2021/02/24 16:59:46 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,333 +77,345 @@
 /*
 **=============				basic data types definitions		  =============
 */
-typedef struct			s_rgb
+typedef struct				s_rgb
 {
-	double				red;
-	double				green;
-	double				blue;
-}						t_rgb;
+	double					red;
+	double					green;
+	double					blue;
+}							t_rgb;
 
-typedef struct			s_screen
+typedef struct				s_screen
 {
-	t_coord_3d			u;
-	t_coord_3d			v;
-	t_coord_3d			w;
-	double				width;
-	double				height;
-}						t_screen;
+	t_coord_3d				u;
+	t_coord_3d				v;
+	t_coord_3d				w;
+	double					width;
+	double					height;
+}							t_screen;
 
-typedef struct			s_pixel
+typedef struct				s_pixel
 {
-	double				width;
-	double				height;
-	t_coord_3d			u;
-	t_coord_3d			v;
-}						t_pixel;
+	double					width;
+	double					height;
+	t_coord_3d				u;
+	t_coord_3d				v;
+}							t_pixel;
 
-typedef struct			s_map
+typedef struct				s_map
 {
-	double				width;
-	double				height;
-	double				u_scale;
-	double				v_scale;
-	char				*img_addr;
-	void				*img_ptr;
-	int					bpp;
-	int					line_size;
-}						t_map;
+	double					width;
+	double					height;
+	double					u_scale;
+	double					v_scale;
+	char					*img_addr;
+	void					*img_ptr;
+	int						bpp;
+	int						line_size;
+}							t_map;
 
-typedef struct			s_texture
+typedef struct				s_texture
 {
-	double				width;
-	double				height;
-	double				grid_len;
-	t_rgb				rgb1;
-	t_rgb				rgb2;
-	t_map				uv_map;
-	t_map				bump_map;
-	char				type[2];
-}						t_texture;
+	double					width;
+	double					height;
+	double					grid_len;
+	t_rgb					rgb1;
+	t_rgb					rgb2;
+	t_map					uv_map;
+	t_map					bump_map;
+	char					type[2];
+}							t_texture;
 
 /*
 **=============				scene parameters definitions		  =============
 */
-typedef struct			s_resolution
+typedef struct				s_resolution
 {
-	int					width;
-	int					height;
-}						t_resolution;
+	int						width;
+	int						height;
+}							t_resolution;
 
-typedef struct			s_ambient
+typedef struct				s_ambient
 {
-	double				ratio;
-	t_rgb				rgb;
-}						t_ambient;
+	double					ratio;
+	t_rgb					rgb;
+}							t_ambient;
 
-typedef struct			s_camera
+typedef struct				s_camera
 {
-	t_pixel				pixel;
-	t_pixel				sub_pixel;
-	t_screen			screen;
-	t_coord_3d			position;
-	struct s_camera		*previous;
-	struct s_camera		*next;
-	int					fov;
-}						t_camera;
+	t_pixel					pixel;
+	t_pixel					sub_pixel;
+	t_screen				screen;
+	t_coord_3d				position;
+	struct s_camera			*previous;
+	struct s_camera			*next;
+	int						fov;
+}							t_camera;
 
-typedef struct			s_light
+typedef struct				s_light
 {
-	double				brightness;
-	t_rgb				rgb;
-	t_coord_3d			position;
-	struct s_light		*previous;
-	struct s_light		*next;
-}						t_light;
+	double					brightness;
+	t_rgb					rgb;
+	t_coord_3d				position;
+	struct s_light			*previous;
+	struct s_light			*next;
+}							t_light;
 
 /*
 **=============				prime ojects definitions			  =============
 */
-typedef struct			s_sphere
+typedef struct				s_sphere
 {
-	double				radius;
-	t_3d_basis			basis;
-	t_coord_3d			center;
-	struct s_sphere		*next;
-	t_texture			texture;
-	char				mode;
-}						t_sphere;
+	double					radius;
+	t_3d_basis				basis;
+	t_coord_3d				center;
+	struct s_sphere			*next;
+	t_texture				texture;
+	char					mode;
+}							t_sphere;
 
-typedef	struct			s_plane
+typedef	struct				s_plane
 {
-	t_3d_basis			basis;
-	t_coord_3d			position;
-	struct s_plane		*next;
-	t_texture			texture;
-}						t_plane;
+	t_3d_basis				basis;
+	t_coord_3d				position;
+	struct s_plane			*next;
+	t_texture				texture;
+}							t_plane;
 
-typedef struct			s_square
+typedef struct				s_square
 {
-	double				radius;
-	t_3d_basis			basis;
-	t_coord_3d			position;
-	void				*parent;
-	struct s_square		*next;
-	t_texture			texture;
-	int					parent_type;
-	int					face_number;
-}						t_square;
+	double					radius;
+	t_3d_basis				basis;
+	t_coord_3d				position;
+	void					*parent;
+	struct s_square			*next;
+	t_texture				texture;
+	int						parent_type;
+	int						face_number;
+}							t_square;
 
-typedef struct			s_cylinder
+typedef struct				s_cylinder
 {
-	double				radius;
-	double				height;
-	t_coord_3d			position;
-	t_3d_basis			basis;
-	struct s_cylinder	*next;
-	t_texture			texture;
-	int					caps;
-}						t_cylinder;
+	double					radius;
+	double					height;
+	t_coord_3d				position;
+	t_3d_basis				basis;
+	struct s_cylinder		*next;
+	t_texture				texture;
+	int						caps;
+}							t_cylinder;
 
-typedef struct			s_triangle
+typedef struct				s_triangle
 {
-	t_3d_basis			basis;
-	t_coord_3d			c;
-	t_coord_3d			p1;
-	t_coord_3d			p2;
-	t_coord_3d			p3;
-	t_coord_3d			centroid;
-	void				*parent;
-	struct s_triangle	*next;
-	t_texture			texture;
-}						t_triangle;
+	t_3d_basis				basis;
+	t_coord_3d				c;
+	t_coord_3d				p1;
+	t_coord_3d				p2;
+	t_coord_3d				p3;
+	t_coord_3d				centroid;
+	void					*parent;
+	struct s_triangle		*next;
+	t_texture				texture;
+}							t_triangle;
 
-typedef struct			s_cone
+typedef struct				s_cone
 {
-	double				angle;
-	double				height;
-	t_3d_basis			basis;
-	t_coord_3d			vertex;
-	struct s_cone		*next;
-	t_texture			texture;
-}						t_cone;
+	double					angle;
+	double					height;
+	t_3d_basis				basis;
+	t_coord_3d				vertex;
+	struct s_cone			*next;
+	t_texture				texture;
+}							t_cone;
 
-typedef struct			s_disk
+typedef struct				s_disk
 {
-	double				radius;
-	t_3d_basis			basis;
-	t_coord_3d			center;
-	struct s_disk		*next;
-	t_texture			texture;
-}						t_disk;
+	double					radius;
+	t_3d_basis				basis;
+	t_coord_3d				center;
+	struct s_disk			*next;
+	t_texture				texture;
+}							t_disk;
 
-typedef struct			s_rectangle
+typedef struct				s_rectangle
 {
-	double				width;
-	double				height;
-	t_3d_basis			basis;
-	t_coord_3d			position;
-	void				*parent;
-	struct s_square		*next;
-	t_texture			texture;
-	int					parent_type;
-	int					face_number;
-}						t_rectangle;
+	double					width;
+	double					height;
+	t_3d_basis				basis;
+	t_coord_3d				position;
+	void					*parent;
+	struct s_rectangle		*next;
+	t_texture				texture;
+	int						parent_type;
+	int						face_number;
+}							t_rectangle;
 
 /*
 **=============				compound ojects definitions			  =============
 */
-typedef struct			s_cube
+typedef struct				s_cube
 {
-	double				radius;
-	t_3d_basis			basis;
-	t_coord_3d			center;
-	t_square			*faces[6];
-	struct s_cube		*next;
-	t_texture			texture;
-}						t_cube;
+	double					radius;
+	t_3d_basis				basis;
+	t_coord_3d				center;
+	t_square				*faces[6];
+	struct s_cube			*next;
+	t_texture				texture;
+}							t_cube;
 
-typedef struct			s_pyramid
+typedef struct				s_pyramid
 {
-	t_coord_3d			apex;
-	t_square			*base;
-	t_triangle			*faces[4];
-	struct s_pyramid	*next;
-	t_texture			texture;
-}						t_pyramid;
+	t_coord_3d				apex;
+	t_square				*base;
+	t_triangle				*faces[4];
+	struct s_pyramid		*next;
+	t_texture				texture;
+}							t_pyramid;
 
-typedef	struct			s_skybox
+typedef struct				s_hyperrectangle
 {
-	t_square			faces[6];
-}						t_skybox;
+	double					width;
+	double					height;
+	t_3d_basis				basis;
+	t_coord_3d				center;
+	t_rectangle				*faces[6];
+	struct s_hyperrectangle	*next;
+	t_texture				texture;
+}							t_hyperrectangle;
+
+typedef	struct				s_skybox
+{
+	t_square				faces[6];
+}							t_skybox;
 
 /*
 **=============					filters definitions				  =============
 */
-typedef struct			s_sepia
+typedef struct				s_sepia
 {
-	char				on;
-}						t_sepia;
+	char					on;
+}							t_sepia;
 
-typedef struct			s_anti_aliasing
+typedef struct				s_anti_aliasing
 {
-	int					factor;
-}						t_anti_aliasing;
+	int						factor;
+}							t_anti_aliasing;
 
 /*
 **=============					display definition			  	  =============
 */
-typedef struct			s_display
+typedef struct				s_display
 {
-	void				*mlx_ptr;
-	void				*win_ptr;
-	void				*img_ptr;
-	void				*img_addr;
-	int					bpp;
-	int					local_endian;
-	int					server_endian;
-	int					line_size;
-}						t_display;
+	void					*mlx_ptr;
+	void					*win_ptr;
+	void					*img_ptr;
+	void					*img_addr;
+	int						bpp;
+	int						local_endian;
+	int						server_endian;
+	int						line_size;
+}							t_display;
 
 /*
 **=============				non specific functions				  =============
 */
-int						check_critical_entities(void **entities);
-void					get_screen_pixel(void **entities);
-void					destroy_entities(void **entities);
-void					parse_configuration(void **entities, int fd);
-void					configuration_reader(char *input_file, void **entities);
+int							check_critical_entities(void **entities);
+void						get_screen_pixel(void **entities);
+void						destroy_entities(void **entities);
+void						parse_configuration(void **entities, int fd);
+void						configuration_reader(char *input_file, void **entities);
 
 /*
 **=============					parsing	 functions				  =============
 */
-int						update_start(char *line, int start);
-int						find_next_arg(char *line, int start);
-int						check_end_entity_info(char *line, int start, int ret);
-int						get_entity_info(char *line, void **entities);
+int							update_start(char *line, int start);
+int							find_next_arg(char *line, int start);
+int							check_end_entity_info(char *line, int start, int ret);
+int							get_entity_info(char *line, void **entities);
 
 /*
 **=============					fetching functions				  =============
 */
-int						get_radius(char *line, double *radius, int start);
-int						fetch_rgb(char *line, t_rgb *rgb, int start);
-int						fetch_point_3d(char *line, t_coord_3d *point, \
-						int start);
-int						fetch_vector_3d(char *line, t_coord_3d *vector, \
-						int start);
+int							get_radius(char *line, double *radius, int start);
+int							fetch_rgb(char *line, t_rgb *rgb, int start);
+int							fetch_point_3d(char *line, t_coord_3d *point, \
+							int start);
+int							fetch_vector_3d(char *line, t_coord_3d *vector, \
+							int start);
 
 /*
 **=============					linked lists functions			  =============
 */
-void					ft_lst_add_head(void **lst_tail, void *new, int type);
-void					ft_lst_destroy(void **lst_tail, int type, \
-						t_display *display);
-void					ft_dclst_add_head(void **lst_tail, void *new, int type);
-void					ft_dclst_destroy(void **tail, int type);
-void					*get_next_lst_mem(void *lst_mem, int type);
-void					*get_previous_lst_mem(void *lst_mem, int type);
-void					ft_update_next(void *lst_mem, void *next, int type);
-void					ft_update_previous(void *lst_mem, void *previous, \
-						int type);
-void					ft_destroy_lst_mem(void *lst_mem, int type, \
-						t_display *display);
-t_texture				*get_lst_mem_texture(void *lst_mem, int type);
+void						ft_lst_add_head(void **lst_tail, void *new, int type);
+void						ft_lst_destroy(void **lst_tail, int type, \
+							t_display *display);
+void						ft_dclst_add_head(void **lst_tail, void *new, int type);
+void						ft_dclst_destroy(void **tail, int type);
+void						*get_next_lst_mem(void *lst_mem, int type);
+void						*get_previous_lst_mem(void *lst_mem, int type);
+void						ft_update_next(void *lst_mem, void *next, int type);
+void						ft_update_previous(void *lst_mem, void *previous, \
+							int type);
+void						ft_destroy_lst_mem(void *lst_mem, int type, \
+							t_display *display);
+t_texture					*get_lst_mem_texture(void *lst_mem, int type);
 
 /*
 **=============					texture functions			  	  =============
 */
-int						get_texture(char *line, t_texture *texture, \
-						t_display *display, int type);
-int						get_texture_color(char *line, t_texture *texture, \
-						int start);
-int						get_texture_type(char *line, char *texture_type, \
-						int start);
-int						get_texture_map(char *line, t_map *map, \
-						t_display *display, int start);
+int							get_texture(char *line, t_texture *texture, \
+							t_display *display, int type);
+int							get_texture_color(char *line, t_texture *texture, \
+							int start);
+int							get_texture_type(char *line, char *texture_type, \
+							int start);
+int							get_texture_map(char *line, t_map *map, \
+							t_display *display, int start);
 
-void					initialise_map_dimension(t_texture *texture);
-void					get_triangle_basis(t_triangle *triangle);
-void					prepare_triangle_mapping(t_triangle *triangle);
-void					get_triangle_texture_center(t_triangle *triangle);
+void						initialise_map_dimension(t_texture *texture);
+void						get_triangle_basis(t_triangle *triangle);
+void						prepare_triangle_mapping(t_triangle *triangle);
+void						get_triangle_texture_center(t_triangle *triangle);
 
 /*
 **=============				scene parameters functions			  =============
 */
-int						get_resolution(char *line, void **entities);
-int						get_ambient_light(char *line, void **entities);
-int						get_camera(char *line, void **entities);
-int						get_light(char *line, void **entities);
+int							get_resolution(char *line, void **entities);
+int							get_ambient_light(char *line, void **entities);
+int							get_camera(char *line, void **entities);
+int							get_light(char *line, void **entities);
 
 /*
 **=============				prime objects functions				  =============
 */
-int						get_sphere(char *line, void **entities);
-int						get_plane(char *line, void **entities);
-int						get_square(char *line, void **entities);
-int						get_cylinder(char *line, void **entities);
-int						get_triangle(char *line, void **entities);
-int						get_cone(char *line, void **entities);
-int						get_disk(char *line, void **entities);
+int							get_sphere(char *line, void **entities);
+int							get_plane(char *line, void **entities);
+int							get_square(char *line, void **entities);
+int							get_cylinder(char *line, void **entities);
+int							get_triangle(char *line, void **entities);
+int							get_cone(char *line, void **entities);
+int							get_disk(char *line, void **entities);
+int							get_rectangle(char *line, void **entities);
 
 /*
 **=============					cube functions					  =============
 */
-int						get_cube(char *line, void **entities);
-int						build_cube(t_cube *cube, void **entities);
-int						construct_cubes(t_cube *tail, void **entities);
-void					build_cube_faces(t_square *faces[6], t_coord_3d \
-						position, double radius, t_coord_3d *base);
+int							get_cube(char *line, void **entities);
+int							build_cube(t_cube *cube, void **entities);
+int							construct_cubes(t_cube *tail, void **entities);
+void						build_cube_faces(t_square *faces[6], t_coord_3d \
+							position, double radius, t_coord_3d *base);
 
 /*
 **=============					pyramid functions				  =============
 */
-int						get_pyramid(char *line, void **entities);
-int						build_pyramid(t_pyramid *pyramid, void **entities);
-int						construct_pyramids(t_pyramid *tail, void **entities);
-void					build_pyramid_faces(t_pyramid *pyramid, double radius, \
-						t_coord_3d center);
+int							get_pyramid(char *line, void **entities);
+int							build_pyramid(t_pyramid *pyramid, void **entities);
+int							construct_pyramids(t_pyramid *tail, void **entities);
+void						build_pyramid_faces(t_pyramid *pyramid, double radius, \
+							t_coord_3d center);
 
-int						get_skybox(char *line, void **entities);
-int						get_sepia(char *line, void **entities);
-int						get_anti_aliasing(char *line, void **entities);
+int							get_skybox(char *line, void **entities);
+int							get_sepia(char *line, void **entities);
+int							get_anti_aliasing(char *line, void **entities);
 
 #endif
--- srcs/configuration_reader/get_rectangle.c
+++ srcs/configuration_reader/get_rectangle.c
@@ -6,17 +6,33 @@
 /*   By: yarroubi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 16:31:30 by yarroubi          #+#    #+#             */
-/*   Updated: 2021/02/24 16:33:40 by yarroubi         ###   ########.fr       */
+/*   Updated: 2021/02/24 16:59:06 by yarroubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/configuration_reader.h"
 
-static int	get_dimension(t_rectangle *rectangle, char *line, int start)
+static int	get_dimensions(t_rectangle *rectangle, char *line, int start)
 {
+	int		holder;
+
+	if (!ft_isnumber(line + start))
+		return (-1);
+	rectangle->width = ft_atod_length(line + start, &holder);
+	if (rectangle->width < 0.0)
+		return (-1);
+	start = update_start(line, start + holder);
+	if (start == -1)
+		return (-1);
+	if (!ft_isnumber(line + start))
+		return (-1);
+	rectangle->height = ft_atod_length(line + start, &holder);
+	if (rectangle->width < 0.0)
+		return (-1);
+	return (update_start(line, start + holder));
 }
 
-int	get_rectangle(char *line, void **entities)
+int			get_rectangle(char *line, void **entities)
 {
 	int			start;
 	t_rectangle	*rectangle;
@@ -28,19 +44,20 @@ int	get_rectangle(char *line, void **entities)
 	rectangle->texture.uv_map.img_ptr = 0;
 	rectangle->texture.bump_map.img_ptr = 0;
 	if ((start = update_start(line, 2)) == -1)
-		return (-rectangle);
+		return (-RECTANGLE);
 	start = fetch_point_3d(line, &(rectangle->position), start);
 	if ((start = update_start(line, start)) == -1)
-		return (-rectangle);
+		return (-RECTANGLE);
 	start = fetch_vector_3d(line, &(rectangle->basis.w), start);
 	if ((start = update_start(line, start)) == -1)
-		return (-rectangle);
-	if ((start = get_radius(line, &(rectangle->radius), start)) == -1)
-		return (-rectangle);
-	get_base_3d(&(rectangle->basis.w), &(rectangle->basis.v), &(rectangle->basis.u));
+		return (-RECTANGLE);
+	if ((start == get_dimensions(rectangle, line, start)) == -1)
+		return (-RECTANGLE);
+	get_base_3d(&(rectangle->basis.w), &(rectangle->basis.v), \
+		&(rectangle->basis.u));
 	rectangle->texture.width = rectangle->radius * 2.0;
 	rectangle->texture.height = rectangle->texture.width;
 	initialise_map_dimension(&(rectangle->texture));
-	return (get_texture(line + start, &(rectangle->texture), entities[DISPLAY], \
-		rectangle));
+	return (get_texture(line + start, &(rectangle->texture), \
+		entities[DISPLAY], RECTANGLE));
 }
